9ac8b9953762bde21f1acf9176f1b13365bcf1b8
ConnorMaloney <foopert@gmail.com>
2020-06-27 23:33:16 -0400

    Cooper is back baby

--- cooper.py
+++ cooper.py
@@ -1,3 +1,23 @@
 import speech_recognition as sr
+import pyttsx3
 
+engine = pyttsx3.init()
+while True:
 
+    # obtain audio from the microphone
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        audio = r.listen(source)
+
+    # recognize speech using Google Speech Recognition
+    try:
+        # for testing purposes, we're just using the default API key
+        # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+        # instead of `r.recognize_google(audio)`
+        engine.say(r.recognize_google(audio))
+        engine.runAndWait()
+    except sr.UnknownValueError:
+        print("Google Speech Recognition could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
